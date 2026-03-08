b10d30ff051e39458b01e34781f731a830da3ec1
Jhonatas Melo <140463324+JhonatasMelo@users.noreply.github.com>
2026-03-07 21:44:45 -0300

    Atualizando app.js

--- app.js
+++ app.js
@@ -69,5 +69,10 @@ function reiniciarJogo() {
 
 
 
+/*git init
+git add .
+git commit -m "projeto inicial"
+git remote add origin git@github.com:SEU_USUARIO/SEU_REPOSITORIO
+git push -u origin main */
 
 
